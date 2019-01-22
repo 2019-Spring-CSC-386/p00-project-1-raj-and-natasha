6b5a8bf1c132e781e976c6cdde986497e15344d1
Natasha Stallsmith <31216171+natstall@users.noreply.github.com>
2019-01-22 11:27:27 -0500

    Updated to light up LEDs. Works with our sensor.

--- Soil_Moisture_Project_1.ino
+++ Soil_Moisture_Project_1.ino
@@ -1,6 +1,6 @@
 /*  Natasha Stallsmith and Tirtha Subedi
     Prints soil moisture values to Serial Moniter
-    Lights up red LED if soil is too dry and green LED if soil is still moist
+    Lights up red LED if soil is too dry and green LED if soil is still moist.
 
     Credits:
       Soil Moisture Basic Example by SparkFun Electronics, Joel Bartlett, August 31, 2015
@@ -8,14 +8,12 @@
     
 */
 
-int val = 0; //value for storing moisture value 
+int soilMoisture = 0; //value for storing moisture value 
 int soilPin = A0; //Declare a variable for the soil moisture sensor 
 int soilPower = 7; //Variable for Soil moisture Power
-int thresholdUp = 400; //Value for when the soil is very moist, may need to be calibrated
-int thresholdDown = 250; //Value for when the soil is very dry, may need to be calibrated
+int thresholdUp = 250; //Threshold for when the soil is moist versus dry, may need to be calibrated
 int greenLED = 9; //Pin for output to Green LED
 int redLED = 10; //Pin for output to Red LED
-
 
 //Rather than powering the sensor through the 3.3V or 5V pins, 
 //we'll use a digital pin to power the sensor. This will 
@@ -25,8 +23,10 @@ void setup()
 {
   Serial.begin(9600);   // open serial over USB
 
-  pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
-  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
+  pinMode(soilPower, OUTPUT); //Set power pin as an OUTPUT
+  pinMode(redLED, OUTPUT); //Set redLED pin as OUTPUT
+  pinMode(greenLED, OUTPUT); //Set greenLED pin as OUTPUT
+  digitalWrite(soilPower, LOW); //Set to LOW so no power is flowing through the sensor
 }
 
 void loop() 
@@ -35,24 +35,26 @@ Serial.print("Soil Moisture = ");
 //get soil moisture value from the function below and print it
 soilMoisture = readSoil();
 Serial.print(soilMoisture);
-if (soilMoisture <= thresholdDown){
-  digitalWrite(redLED, HIGH); 
+if (soilMoisture <= threshold){ //if the soil moisture is low, turn off green LED and turn on red LED
+  digitalWrite(redLED, HIGH);
+  digitalWrite(greenLED, LOW);
 }
-else (sensorValue >= thresholdUp){
+else if (soilMoisture >= threshold){ //if the soil moisture is high, turn off red LED and turn on green LED
   digitalWrite(greenLED, HIGH);
+  digitalWrite(redLED, LOW);
 }
-//This 1 second timefrme is used so you can test the sensor and see it change in real-time.
+
+//This 1 second timeframe is used so you can test the sensor and see it change in real-time.
 //For in-plant applications, you will want to take readings much less frequently.
 delay(1000);//take a reading every second
 }
+
 //This is a function used to get the soil moisture content
-int readSoil()
-{
+int readSoil(){
 
-    digitalWrite(soilPower, HIGH);//turn D7 "On"
-    delay(10);//wait 10 milliseconds 
-    val = analogRead(soilPin);//Read the SIG value form sensor 
-    digitalWrite(soilPower, LOW);//turn D7 "Off"
-    return val;//send current moisture value
+    digitalWrite(soilPower, HIGH); //turn D7 "On"
+    delay(10); //wait 10 milliseconds 
+    soilMoisture = analogRead(soilPin); //Read the SIG value form sensor 
+    digitalWrite(soilPower, LOW); //turn D7 "Off"
+    return soilMoisture; //send current moisture value
 }
-
