681f7ace3ea3b06da31f54e601b7feea10c36fce
Natasha Stallsmith <31216171+natstall@users.noreply.github.com>
2019-02-06 19:19:36 -0500

    Updated for solenoid valve.

--- Soil_Moisture_Project_1.ino
+++ Soil_Moisture_Project_1.ino
@@ -1,60 +1,65 @@
 /*  Natasha Stallsmith and Tirtha Subedi
-    Prints soil moisture values to Serial Moniter
+    Prints soil moisture values to Serial Moniter.
     Lights up red LED if soil is too dry and green LED if soil is still moist.
+    Opens valve for one second if soil is too dry.
 
     Credits:
       Soil Moisture Basic Example by SparkFun Electronics, Joel Bartlett, August 31, 2015
       SparkFun Soil Moisture Sensor and Serial LCD example 1 By Sarah Al-Mutlaq 2015
-    
+      Solenoid Valve Example Code Example, BCRobotics, https://www.bc-robotics.com/tutorials/controlling-a-solenoid-valve-with-arduino/ 
 */
 
 int soilMoisture = 0; //value for storing moisture value 
-int soilPin = A0; //Declare a variable for the soil moisture sensor 
-int soilPower = 7; //Variable for Soil moisture Power
-int thresholdUp = 250; //Threshold for when the soil is moist versus dry, may need to be calibrated
+int soilPin = A0; //soil moisture reading pin
+int soilPower = 8; //Soil moisture Power
+int threshold = 250; //Threshold for when the soil is moist versus dry
 int greenLED = 9; //Pin for output to Green LED
 int redLED = 10; //Pin for output to Red LED
-
-//Rather than powering the sensor through the 3.3V or 5V pins, 
-//we'll use a digital pin to power the sensor. This will 
-//prevent corrosion of the sensor as it sits in the soil. 
+int solenoidPin = 4;  //Output pin to solenoid
 
 void setup() 
 {
   Serial.begin(9600);   // open serial over USB
 
   pinMode(soilPower, OUTPUT); //Set power pin as an OUTPUT
+  pinMode(solenoidPin, OUTPUT);  //Set "power" pin as OUTPUT
   pinMode(redLED, OUTPUT); //Set redLED pin as OUTPUT
   pinMode(greenLED, OUTPUT); //Set greenLED pin as OUTPUT
   digitalWrite(soilPower, LOW); //Set to LOW so no power is flowing through the sensor
 }
 
 void loop() 
-{
-Serial.print("Soil Moisture = ");    
+{  
 //get soil moisture value from the function below and print it
 soilMoisture = readSoil();
-Serial.print(soilMoisture);
-if (soilMoisture <= threshold){ //if the soil moisture is low, turn off green LED and turn on red LED
-  digitalWrite(redLED, HIGH);
-  digitalWrite(greenLED, LOW);
+Serial.print("Soil Moisture = "); 
+Serial.print(soilMoisture); 
+Serial.println();
+
+if (soilMoisture <= threshold){ //if the soil moisture is low
+  digitalWrite(redLED, HIGH); //turn on red LED
+  digitalWrite(greenLED, LOW); //turn off green LED
+
+  digitalWrite(solenoidPin, HIGH);    //Switch Solenoid ON
+  delay(1000);                      //Wait 1 Second
+  digitalWrite(solenoidPin, LOW);     //Switch Solenoid OFF
 }
-else if (soilMoisture >= threshold){ //if the soil moisture is high, turn off red LED and turn on green LED
-  digitalWrite(greenLED, HIGH);
-  digitalWrite(redLED, LOW);
+else if (soilMoisture >= threshold){ //if the soil moisture is high
+  digitalWrite(greenLED, HIGH); //turn on green LED
+  digitalWrite(redLED, LOW); //turn off red LED
 }
 
-//This 1 second timeframe is used so you can test the sensor and see it change in real-time.
-//For in-plant applications, you will want to take readings much less frequently.
-delay(1000);//take a reading every second
+//This 1 second timeframe is used so we can test the sensor and see it change in real-time.
+//For in-plant applications, we would take readings much less frequently.
+delay(1000);
 }
 
-//This is a function used to get the soil moisture content
+//This is the function used to get the soil moisture content
 int readSoil(){
 
-    digitalWrite(soilPower, HIGH); //turn D7 "On"
+    digitalWrite(soilPower, HIGH); //turn on power to sensor
     delay(10); //wait 10 milliseconds 
     soilMoisture = analogRead(soilPin); //Read the SIG value form sensor 
-    digitalWrite(soilPower, LOW); //turn D7 "Off"
-    return soilMoisture; //send current moisture value
+    digitalWrite(soilPower, LOW); //turn off power to sensor
+    return soilMoisture; //return current moisture value
 }
